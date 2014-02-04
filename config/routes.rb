9f96919d76858efdfa066805da315c8b23644e77
Ben Angel <benjamin.b2@gmail.com>
2014-02-04 05:43:36 -0800

    added devise config to config/initializers and locals

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 HabitHub::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  devise_for :users
+  root :to => "home#index"
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
