a3e2a6cff96c78f48125d9507b07535a5a9304f9
Ben Angel <benjamin.b2@gmail.com>
2014-02-06 05:39:30 -0800

    add spec and validations for Update model

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,4 +1,8 @@
 class Update < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :tags
+
+  validates_presence_of :content
+  validates_presence_of :twilio_body
+  validates_presence_of :duration
 end
--- spec/models/update_spec.rb
+++ spec/models/update_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Update do
   it { should belong_to :user }
   it { should have_and_belong_to_many :tags }
+
+  it { should validate_presence_of :content}
+  it { should validate_presence_of :twilio_body}
+  it { should validate_presence_of :duration}
 end
