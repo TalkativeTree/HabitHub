f85955de6a9c976caee1afa10d08e29f978f6cb6
Ben Angel <benjamin.b2@gmail.com>
2014-02-06 04:56:58 -0800

    association tests for models

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
 describe Tag do
-  it "should have working associations" do
-    it { should have_and_belong_to_many :updates }
-  end
+  it { should have_and_belong_to_many :updates }
 end
