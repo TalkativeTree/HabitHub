98a6de42618393062b6fd6e057b38231cf65bac1
Ben Angel <benjamin.b2@gmail.com>
2014-02-06 04:55:59 -0800

    sepc for tag & update controllers and controller helpers

--- spec/helpers/update_helper_spec.rb
+++ spec/helpers/update_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
-# the UpdatesControllerHelper. For example:
+# the UpdateHelper. For example:
 #
-# describe UpdatesControllerHelper do
+# describe UpdateHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-describe UpdatesControllerHelper do
+describe UpdateHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have working associations" do
+    it { should have_and_belong_to_many :updates }
+  end
 end
