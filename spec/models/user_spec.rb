e60cee0cdc9a5f495e44d9e48e5e6e9f05ec9fe5
Ben Angel <benjamin.b2@gmail.com>
2014-02-06 23:46:50 -0800

    add email validation to user spec. Uniqueness validation currently conflicting with phony_rails. Submited pull request to phony_rails to add uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,18 @@ describe User do
   it { should validate_uniqueness_of :email }
   it { should validate_presence_of   :email }
 
-  it { should validate_uniqueness_of :cellphone }
   it { should validate_presence_of   :cellphone }
+  it { should validate_uniqueness_of :cellphone }
+
+  context "email validation" do
+
+    it { should_not allow_value("test").for(:email) }
+    it { should_not allow_value("test.@3.").for(:email) }
+    it { should_not allow_value("test.@gmail.sad.com").for(:email) }
+
+    it "should allow only valid emails" do
+      user = FactoryGirl.build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
