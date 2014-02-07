795c108d7cd0a6df4c64469585c8de7d42c137fe
Ben Angel <benjamin.b2@gmail.com>
2014-02-07 01:28:52 -0800

    add email format validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of   :email
   validates_uniqueness_of :email
+  validates_format_of :email, with: /@/, on: :create
 
   phony_normalized_method :cellphone
   phony_normalize :cellphone, default_country_code: 'US'
