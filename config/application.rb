f9c144ec8f3cb55e061bd6e8d5fa8a66ee0e4cb1
Ben Angel <benjamin.b2@gmail.com>
2014-02-06 23:45:40 -0800

    att to.s to value when loading env_local.yml

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module HabitHub
     # config.i18n.default_locale = :de
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+      YAML.load(File.open(env_file))[Rails.env].each do |key, value|
+        ENV[key.to_s] = value.to_s
       end if File.exists?(env_file)
     end
   end
