005a6a0e9291ca0c852ef9d5e380715c6b3a12a4
Ben Angel <benjamin.b2@gmail.com>
2014-02-07 23:11:38 -0800

    add PlanController#index and #create

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -1,2 +1,24 @@
 class PlanController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.user = current_user
+
+    if @plan.save_with_payment
+      redirect_to root_path, notice: "Thank you for subscribing!"
+    else
+      render :index
+    end
+  end
+
+private
+  def plan_params
+    params.require(:plan).permit(:stripe_card_token)
+  end
 end
+
