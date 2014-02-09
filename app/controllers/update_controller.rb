ef4823221efa3f2154c1f51b99af6e72ee04be5e
Ben Angel <benjamin.b2@gmail.com>
2014-02-08 23:55:57 -0800

    generated update controller, spec and view

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -1,2 +1,27 @@
 class UpdateController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  before_action :validate_user, only: [:create]
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @updates = current_user.updates.order('created_at DESC').load.select { |p| p.created_at.day == @date.day }
+  end
+
+  def create
+    @user.updates.create(content: params['Body'], twilio_body: params.except(:action, :controller))
+
+    render nothing: true
+  end
+
+  def commands
+    render :commands
+  end
+
+private
+  def validate_user
+    cellphone_number = PhonyRails.normalize_number(params['From'], default_country_code: 'US')
+    @user = User.find_by_cellphone_number(cellphone_number)
+  end
 end
+
--- spec/controllers/update_controller_spec.rb
+++ spec/controllers/update_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe UpdateController do
+  describe "POST #updates" do
+    before { post :create}
 
+    it { should render_template :nothing}
+  end
 end
