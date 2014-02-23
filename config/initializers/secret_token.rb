877581d4bdaa3b2708b98a38c55ab071bb4e27d6
Ben Angel <benjamin.b2@gmail.com>
2014-02-23 12:04:52 -0800

    rails new HabitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HabitHub::Application.config.secret_key_base = '3206f5473ad410f93fdae712649287d6faa903f150fb0ee4670bb8c816212df252ac1393eae8f8706c5a53918cf610667d2050ee88af715079b031b13caa0c74'
